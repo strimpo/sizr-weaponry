0ce5a5b78cefb50cbd074755c0c40555f9a78650
Witheredaspect <eepyboat@gmail.com>
2025-08-20 19:44:20 -0500

    base

---
+++ src/client/java/com/example/sizr/SizrClient.java
@@ -0,0 +1,10 @@
+package com.example.sizr;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SizrClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
